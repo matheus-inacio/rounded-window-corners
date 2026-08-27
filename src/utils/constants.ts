60e9a0c7bc1e57db66ef485abbeaed82b88fb4eb
Matheus Inácio <29243277+matheus-inacio@users.noreply.github.com>
2026-08-26 23:33:39 -0300

    refactor: remove unused CLIP_SHADOW_EFFECT constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,8 +3,5 @@
 /** Name of the rounded corners effect */
 export const ROUNDED_CORNERS_EFFECT = 'Rounded Corners Effect';
 
-/** Name of the shadow clipping effect  */
-export const CLIP_SHADOW_EFFECT = 'Clip Shadow Effect';
-
 /** Padding of shadow actors */
 export const SHADOW_PADDING = 80;
